df4741a58eb3661f404b2d4fa80d108d9b07007e
shoo <supershoo3@gmail.com>
2023-08-28 12:53:52 -0500

    fixed store issue, going t oneed to change seed data little.

--- react-app/src/components/Server/server.js
+++ react-app/src/components/Server/server.js
@@ -1,12 +1,15 @@
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect } from "react"
 import { getServers } from "../../store/server"
+import { getServer } from "../../store/server"
 
 function Server(){
 
   const dispatch = useDispatch()
   const user = useSelector(state => state.session.user)
-  const servers = useSelector(state => state.servers)
+  const servers = useSelector(state => state.server)
+  const serversArr = Object.values(servers)
+  console.log(serversArr)
 
   useEffect(() => {
     dispatch(getServers(user.id))
@@ -14,8 +17,18 @@ function Server(){
 
 
   return (
-    <div>
-      <p>hi</p>
+    <div className='container'>
+      <div className='vertical_display'>
+        <div className='servers'>
+          {serversArr.map((server) => {
+            return (
+              <li key={server.id}>
+                <p>{server.servers.img_url}</p>
+              </li>
+            )
+          })}
+        </div>
+      </div>
     </div>
   )
 }
